7e2c40cfe79f975a69765853e0221e3f1ab8ad81
Iyan <iyanfef@gmail.com>
2022-10-25 09:41:10 +0200

    constructor coche(modelo,matricula)

--- src/Coche.java
+++ src/Coche.java
@@ -3,4 +3,11 @@ public class Coche {
 
 	private String modelo;
 	private String matricula;
+	public Coche(String modelo, String matricula) {
+		super();
+		this.modelo = modelo;
+		this.matricula = matricula;
+	}
+	
+	
 }
