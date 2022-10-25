6a47c41062958d7a34a1303ada5f509db968fd2b
Iyan <iyanfef@gmail.com>
2022-10-25 10:17:45 +0200

    avance 1

--- src/Start.java
+++ src/Start.java
@@ -3,12 +3,13 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-<<<<<<< HEAD
+
 		System.out.println("Master hola");
 
-=======
+
 		System.out.println("Personas hola");
->>>>>>> refs/heads/personas
+		System.out.println("avance 1");
+
 	}
 
 }
